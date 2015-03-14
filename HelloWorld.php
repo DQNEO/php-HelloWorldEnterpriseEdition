f3d33360eb03db2e8b924ab51271b720a72913a0
DQNEO <dqneoo@gmail.com>
2015-03-14 22:55:07 +0900

    enable injection from constructor

--- HelloWorld.php
+++ HelloWorld.php
@@ -3,10 +3,18 @@ namespace DQNEO;
 
 class HelloWorld
 {
+
+    private $string;
+
     public static function main()
     {
         $obj = new static();
         $obj->say();
+    }
+
+    public function __construct($string = null)
+    {
+        $this->string = $string;
     }
 
     public function say()
