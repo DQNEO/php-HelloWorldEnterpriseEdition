bff73ca804dcfeec5104456617c366ec32106dbb
DQNEO <dqneoo@gmail.com>
2015-03-15 00:18:04 +0900

    depends on interface, not on implementation

--- DQNEO/HelloWorld/HelloWorldApplication.php
+++ DQNEO/HelloWorld/HelloWorldApplication.php
@@ -6,7 +6,7 @@ class HelloWorldApplication
     private $string;
     private $writer;
 
-    public function __construct(String $string, Writer $writer)
+    public function __construct(String $string, WriterInterface $writer)
     {
         $this->string = $string;
         $this->writer = $writer;
