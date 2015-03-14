fbdc95538e73fe2545c1c23f1c3c8de9298fb229
DQNEO <dqneoo@gmail.com>
2015-03-14 23:18:52 +0900

    separate class loader class file

--- autoload.php
+++ autoload.php
@@ -1,15 +1,6 @@
 <?php
+use DQNEO\HelloWorld\ClassLoader;
 
-spl_autoload_register(function ($c) {
-    $lastSlash = strrpos($c, '\\');
-    if (false !== $lastSlash) {
-        $lastSlash++;
-        $ns = substr($c, 0, $lastSlash);
-        $class = substr($c, $lastSlash);
-        $path = strtr($ns, '\\', '/') . strtr($class, '_', '/');
-    } else {
-        $path = strtr($c, '_', '/');
-    }
-    @include_once $path . '.php';
-});
+require_once __DIR__ . '/DQNEO/HelloWorld/ClassLoader.php';
 
+ClassLoader::register();
