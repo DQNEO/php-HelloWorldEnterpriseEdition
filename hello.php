59211912ad3600c511f96ec6f5d3732204ceff39
DQNEO <dqneoo@gmail.com>
2015-03-14 23:05:56 +0900

    move dir

--- hello.php
+++ hello.php
@@ -1,6 +1,6 @@
 #!/usr/bin/env php
 <?php
-require_once __DIR__ . '/HelloWorld.php';
-require_once __DIR__ . '/String.php';
+require_once __DIR__ . '/DQNEO/HelloWorld.php';
+require_once __DIR__ . '/DQNEO/String.php';
 
 \DQNEO\HelloWorld::main();
