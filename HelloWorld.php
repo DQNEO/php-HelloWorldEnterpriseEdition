c50274d7d6d15850b3dfb4de1f071e520193b562
DQNEO <dqneoo@gmail.com>
2015-03-14 22:52:48 +0900

    OOP style

--- HelloWorld.php
+++ HelloWorld.php
@@ -5,6 +5,12 @@ class HelloWorld
 {
     public static function main()
     {
+        $obj = new static();
+        $obj->say();
+    }
+
+    public function say()
+    {
         echo "hello world\n";
     }
 }
