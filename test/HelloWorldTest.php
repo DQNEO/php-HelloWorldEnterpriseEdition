9f8a99df60ebd7c967169531db0e63784d32b6f9
DQNEO <dqneoo@gmail.com>
2015-03-15 00:47:48 +0900

    add correct test

--- test/HelloWorldTest.php
+++ test/HelloWorldTest.php
@@ -1,8 +1,33 @@
 <?php
+use DQNEO\HelloWorld\HelloWorldApplication;
+use DQNEO\HelloWorld\String;
+use DQNEO\HelloWorld\Writer\WriterInterface;
+
+class MockWriter implements WriterInterface
+{
+    public $buf = "";
+
+    public function write(String $string)
+    {
+        $this->buf .= $string->getValue();
+    }
+
+    public function writeLn(String $string)
+    {
+        $this->buf .= $string->getValue(). "\n";
+    }
+
+}
+
 class HelloWorldTest extends \PHPUnit_Framework_TestCase
 {
-    public function testFoo()
+    public function testHelloWorld()
     {
-        $this->assertEquals(1, 1);
+        $string = new String("hello world");
+        $writer = new MockWriter;
+        $app = new HelloWorldApplication($string, $writer);
+        $app->run();
+        $this->assertEquals("hello world\n", $writer->buf);
     }
+
 }
