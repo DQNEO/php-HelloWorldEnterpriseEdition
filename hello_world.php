0e2a42fe98b440cbc9b7af0ed0e20aaa266f80a8
DQNEO <dqneoo@gmail.com>
2015-03-16 22:52:45 +0900

    fix to work for psr4

--- hello_world.php
+++ hello_world.php
@@ -1,5 +1,5 @@
 #!/usr/bin/env php
 <?php
-require_once __DIR__ . '/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-\DQNEO\HelloWorld\CLI::main();
+\DQNEO\HelloWorldEnterpriseEdition\CLI::main();
